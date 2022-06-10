78bdcefeac18e7da71f669f924d564693f238f0a
Maxi Bartolozzi <maxibartolozzi@gmail.com>
2022-06-09 21:21:08 -0300

    add an error message when the length is empty

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ let generatePw = document.getElementById("generate-pw")
 let pwLength = document.getElementById("pw-length")
 let pwBoxes = document.querySelectorAll(".pw-box")
 let randomCharacter = ""
-
+let copyPw = document.getElementsByClassName("copy-pw")
 
 
 function generatePassword(){
-    for(let c = 0; c < pwBoxes.length; c++){
-        for(let i = 0; i < pwLength.value; i++){
-            let getRandomCharacter = Math.floor(Math.random()* pwCharacters.length)
-            randomCharacter += pwCharacters[getRandomCharacter]
+    console.log(pwLength.value)
+    if(pwLength.value !== ""){
+        for(let c = 0; c < pwBoxes.length; c++){
+            for(let i = 0; i < pwLength.value; i++){
+                let getRandomCharacter = Math.floor(Math.random()* pwCharacters.length)
+                randomCharacter += pwCharacters[getRandomCharacter]
+            }
+            pwBoxes[c].value = randomCharacter
+            copyPw[c].classList.add("enabled")
+            randomCharacter = ""
         }
-        pwBoxes[c].value = randomCharacter
-        randomCharacter = ""
+        pwLength.value = ""
+        document.getElementById("alert-length").classList.remove('alert-message')
+    } else{
+        document.getElementById("alert-length").className = 'alert-message'
     }
-    pwLength.value = ""
+}
+
+function copyPassword(){
+
 }
